476cc5282f8e3ed8a6a7115cc075a76a86bd1208
Tom Forsell <77614564+TomForsell@users.noreply.github.com>
2021-02-02 16:06:29 +0100

    Finished regular expressions lab work

--- src/dnb/reskill/tom/RegexDemo.java
+++ src/dnb/reskill/tom/RegexDemo.java
@@ -1,8 +1,4 @@
 package dnb.reskill.tom;
-
-
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,7 +9,8 @@ public class RegexDemo {
 
 
         //a series of regular expression tests
-        System.out.println(isMatching(".x.a.","yxpar"));//test . which stands for any character (true)
+
+       /*System.out.println(isMatching(".x.a.","yxpar"));//test . which stands for any character (true)
         System.out.println(isMatching(".x.a.","yapar"));//test . which stands for any character (false)
         System.out.println(isMatching("[agh]?","a"));//test that ag or h occurs, but only once and one char (true)
         System.out.println(isMatching("[a]?","ag"));//test that a occurs, but only once and one char (false)
@@ -22,8 +19,17 @@ public class RegexDemo {
         System.out.println(Pattern.matches("\\d","1")); //test for a digit (true)
         System.out.println(Pattern.matches("\\d","11")); //test for a digit (false)
         System.out.println(Pattern.matches("[\\d]{3}\\Dx?y?","188dy")); //test for a little more complex regex (true)
+        */
+        System.out.println((Pattern.matches("[\\D]+[\\d]{3}$","hallaisiken132"))); // TRUE
 
 
+
+
+
+        //repeatedFinds("","/hallo/hallaisiken132/hello");
+        //repeatedFinds("/hallo/hallaisiken132/hello", "[\\D][\\d]{3}$");
+        replaceText("hello mum", "[a-z]+");
+        usingStringMethods();
 
     }
     public static boolean isMatching(String regex, String stringToTest){
@@ -32,6 +38,43 @@ public class RegexDemo {
         return matcher.matches();
     }
 
+    public static void repeatedFinds(CharSequence sequence, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(sequence);
+
+        System.out.printf("\n find() calls on %s for regex %s:\n", sequence, regex);
+        int numberOfGroups = matcher.groupCount();
+        System.out.println("number of groups:" + numberOfGroups);
+        //TODO : ASK ANDY
+        while(matcher.find()){
+            System.out.println("" + matcher.group());
+        }
+    }
+
+    public static void replaceText (CharSequence inputSequence, String regex){
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(inputSequence);
+        String replaceWith = "HaLLaisiken";
+        if (m.find()){
+            System.out.println("it matches " );
+            System.out.println("String after first replacement " + m.replaceFirst(replaceWith));
+        }
+        m.replaceAll(replaceWith);
+        System.out.println("String after replaceAll:" + replaceWith);
+    }
+
+    public static void usingStringMethods(){
+        String testString = "5034 Bergen";
+        String regex = "\\d{4}\\s\\D{6}";
+        if(testString.matches(regex))
+            System.out.println("5034 Bergen matches with " + regex );
+        String stringWithDividers = "Hi! how are you today? The weather, its been fantastic!";
+        String[] dividerComponents = stringWithDividers.split("[!?,]"); //TODO ASK for explanation of brackets
+        for (String pc : dividerComponents) {
+            System.out.println(pc);
+        }
+        System.out.println();
+    }
 
 
 }
