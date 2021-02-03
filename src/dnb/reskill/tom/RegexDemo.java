e8fbb2283f86280bc834cf4c12e2c0f05ae47cf8
Tom Forsell <77614564+TomForsell@users.noreply.github.com>
2021-02-03 13:46:11 +0100

    Finished Collections lab. TODO: test and refactor

--- src/dnb/reskill/tom/RegexDemo.java
+++ src/dnb/reskill/tom/RegexDemo.java
@@ -10,7 +10,7 @@ public class RegexDemo {
 
         //a series of regular expression tests
 
-       System.out.println(isMatching(".x.a.","yxpar"));//test . which stands for any character (true)
+     /*  System.out.println(isMatching(".x.a.","yxpar"));//test . which stands for any character (true)
         System.out.println(isMatching(".x.a.","yapar"));//test . which stands for any character (false)
         System.out.println(isMatching("[agh]?","a"));//test that ag or h occurs, but only once and one char (true)
         System.out.println(isMatching("[a]?","ag"));//test that a occurs, but only once and one char (false)
@@ -18,17 +18,12 @@ public class RegexDemo {
         System.out.println(isMatching("[a]+","h"));//test that a occurs one or more then one time (false)
         System.out.println(Pattern.matches("\\d","1")); //test for a digit (true)
         System.out.println(Pattern.matches("\\d","11")); //test for a digit (false)
-        System.out.println(Pattern.matches("[\\d]{3}\\Dx?y?","188dy")); //test for a little more complex regex (true)
+        System.out.println(Pattern.matches("[\\d]{3}\\Dx?y?","188dy")); //test for a little more complex regex (true)*/
         System.out.println((Pattern.matches("[\\D]+[\\d]{3}$","hallaisiken132"))); // TRUE
-
-
-
-
-
         //repeatedFinds("","/hallo/hallaisiken132/hello");
-        //repeatedFinds("/hallo/hallaisiken132/hello", "[\\D][\\d]{3}$");
+        //repeatedFinds("/hallo/hallaisiken132/hello", "[a-z]+\\d{3}");
         replaceText("hello mum", "[a-z]+");
-        usingStringMethods();
+        //usingStringMethods();
 
     }
     public static boolean isMatching(String regex, String stringToTest){
@@ -42,11 +37,11 @@ public class RegexDemo {
         Matcher matcher = pattern.matcher(sequence);
 
         System.out.printf("\n find() calls on %s for regex %s:\n", sequence, regex);
-        int numberOfGroups = matcher.groupCount();
-        System.out.println("number of groups:" + numberOfGroups);
+        //int numberOfGroups = matcher.groupCount();
+        //System.out.println("number of groups:" + numberOfGroups);
         //TODO : ASK ANDY
         while(matcher.find()){
-            System.out.println("" + matcher.group());
+            System.out.println("matching string " + matcher.group());
         }
     }
 
@@ -61,8 +56,8 @@ public class RegexDemo {
         m.replaceAll(replaceWith);
         System.out.println("String after replaceAll:" + replaceWith);
     }
-
     public static void usingStringMethods(){
+        //I had to look at solution code for this one
         String testString = "5034 Bergen";
         String regex = "\\d{4}\\s\\D{6}";
         if(testString.matches(regex))
@@ -74,6 +69,4 @@ public class RegexDemo {
         }
         System.out.println();
     }
-
-
 }
